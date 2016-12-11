2dcefe6a08f17b177be1c9ec86e9d397f8f2b939
manistra <lor3nzo95@gmail.com>
2016-12-11 15:10:28 +0100

    fixcom

--- skriptica.js
+++ skriptica.js
@@ -1,7 +1,7 @@
 var appModule = angular.module("ap1", []);
 
 appModule.controller("mController", function ($scope) {
-	// kod koji se bude nalazio unutar tijela ove funkcije je kod 'mController' kontrolera
+
 
 
 	$scope.br1 = "br1";
