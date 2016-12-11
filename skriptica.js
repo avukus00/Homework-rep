3ec1057174de086118af2b6e4f7463c4162322c5
manistra <lor3nzo95@gmail.com>
2016-12-11 15:24:53 +0100

    com2

--- skriptica.js
+++ skriptica.js
@@ -1,15 +1,11 @@
 var appModule = angular.module("ap1", []);
 
 appModule.controller("mController", function ($scope) {
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> b3a35d6312db37def7d5184fff5c6694219652ec
 
 
-	$scope.br1 = "br1";
-	$scope.br2 = "br2";
+
+	$scope.br1 = "";
+	$scope.br2 = "";
 
 	
 	
