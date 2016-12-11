7177bec3ce7258be7141ed5b0d9263b0637e4d4d
manistra <lor3nzo95@gmail.com>
2016-12-11 15:06:00 +0100

    unfinished-homowork-com

---
+++ skriptica.js
@@ -0,0 +1,12 @@
+var appModule = angular.module("ap1", []);
+
+appModule.controller("mController", function ($scope) {
+	// kod koji se bude nalazio unutar tijela ove funkcije je kod 'mController' kontrolera
+
+
+	$scope.br1 = "br1";
+	$scope.br2 = "br2";
+
+	
+	
+});
