2c2e962c473872fc9a1903776221c87a498fbe76
manistra <lor3nzo95@gmail.com>
2016-12-20 23:52:18 +0100

    unfinished hom.4

--- profileController.js
+++ profileController.js
@@ -1,5 +1,11 @@
 appModule.controller("profileController", function ($scope) {
-	$scope.inventarInp = "";
+	
+	$scope.imeInp="";
+	$scope.brojInp="";
+	$scope.vrijednostInp="";
+	$scope.opisInp="";
+	$scope.tempDost="";
+
 	$scope.profile = 
 		{
 			ime: "",
@@ -8,18 +14,45 @@ appModule.controller("profileController", function ($scope) {
 			items: []			
 		};
 		
+	
 	$scope.addToArray = function() {
-	        if ($scope.inventarInp) {
-	          $scope.profile.items.push(this.inventarInp);
-	          $scope.inventarInp = '';
-	        };
-}	        
+	$scope.profile.items.push({
+		ime : $scope.imeInp,
+		broj : $scope.brojInp,
+		value : $scope.vrijednostInp,
+		opis : $scope.opisInp,
+		dost : $scope.dostuponstInp
+		});
+		$scope.imeInp="";
+		$scope.brojInp="";
+		$scope.vrijednostInp="";
+		$scope.opisInp="";
+		$scope.dostuponstInp= false;
+	};
+
+    $scope.condition = function() {
+		if ($scope.brojInp > 10000 || $scope.brojInp < 1 || ($scope.imeInp == '')) 
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}		
+		}; 
+
+	$scope.condition1 = function() {
+		if ($scope.brojInp > 10000 || $scope.brojInp < 1) 
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}		
+		}; 
 });
-	
-
-	
-	
-
+	          
 
 
 	
